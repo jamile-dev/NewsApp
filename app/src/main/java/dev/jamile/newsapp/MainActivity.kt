5b002e81506952ddb0df8896a35f4dfdf297bed3
Jamile <jamile.bastos.dev@gmail.com>
2022-07-14 11:16:08 -0300

    chore: add recyclerview setup

--- app/src/main/java/dev/jamile/newsapp/MainActivity.kt
+++ app/src/main/java/dev/jamile/newsapp/MainActivity.kt
@@ -2,10 +2,20 @@ package dev.jamile.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.jamile.newsapp.databinding.ActivityMainBinding
+import dev.jamile.newsapp.domain.model.News
+import dev.jamile.newsapp.newsList.NewsAdapter
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val newsAdapter = NewsAdapter(arrayListOf<News>())
+        binding.recyclerview.adapter = newsAdapter
     }
 }
--- app/src/main/java/dev/jamile/newsapp/newsList/NewsAdapter.kt
+++ app/src/main/java/dev/jamile/newsapp/newsList/NewsAdapter.kt
@@ -1,18 +1,14 @@
 package dev.jamile.newsapp.newsList
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ListAdapter
-import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import dev.jamile.newsapp.databinding.NewsItemBinding
-import dev.jamile.newsapp.model.News
+import dev.jamile.newsapp.domain.model.News
 
-class NewsAdapter(private val newsList: List<Any>) :
-    RecyclerView.Adapter<NewsAdapter.HoursViewHolder>() {
+class NewsAdapter(private val newsList: List<News>) :
+    RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
         val binding = NewsItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -24,24 +20,23 @@ class NewsAdapter(private val newsList: List<Any>) :
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
         with(holder) {
             with(newsList[position]) {
-                binding.topLearnerName.text = name
-                val hours = "$hours learning hours, $country"
-                binding.topLearnerTime.text = hours
+                binding.let {
+                    it.sourceName.text = this.title
+                    it.description.text = this.description
+                }
+
                 Glide.with(holder.itemView.context)
-                    .load(newsUrl)
-                    .into(binding.topLearnerImage)
+                    .load(urlToImage)
+                    .into(binding.newsImage)
 
                 holder.itemView.setOnClickListener {
-                    Toast.makeText(
-                        holder.itemView.context, hours,
-                        Toast.LENGTH_SHORT
-                    ).show()
+
                 }
             }
         }
     }
 
-    inner class NewsViewHolder(val binding: HoursListItemsBinding) :
+    inner class NewsViewHolder(val binding: NewsItemBinding) :
         RecyclerView.ViewHolder(binding.root)
 
 }
